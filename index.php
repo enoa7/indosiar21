605bf1d92cc14fbb7668983f83ccdb61c1577a69
Gamal Ardi Pratama <gamal.pratama@sctv-jkt.tv>
2015-12-07 18:19:22 +0700

    update 12-07-2015

--- index.php
+++ index.php
@@ -29,9 +29,23 @@
 			<div><img class="img-responsive center-block" src="asset/images/indosiar21-artist-slank.jpg" alt=""></div>
 		</div>
 	</div>
-	<div class="segment">
+	<div class="segment" id="kontes">
 		<div class="container-fluid">
-			<div><img class="img-responsive center-block" src="asset/images/kontes-wefie.jpg" alt=""></div>
+			<?php if ( $detect->isMobile() ) { ?>
+				<div><img class="img-responsive center-block" src="asset/images/kontes-wefie-title.png" alt=""></div>
+			<?php } else { ?>
+				<div><img class="img-responsive center-block" src="asset/images/kontes-wefie.jpg" alt=""></div>
+			<?php } ?>
+		</div>
+	</div>
+	<div class="segment" id="livestream">
+		<div class="container-fluid">
+			<div class="intro-text"><h2>Watch our livestream here!</div>
+				<div class="video col-xs-12">
+					<div class="">
+						<iframe class="vidio-embed" src="http://www.vidio.com/embed/208881-andik-tiru-cr7-jadi-kakek-kakek-main-bola-di-jalan?autoplay=false" scrolling="no" frameborder="0" height="380" allowfullscreen></iframe>
+					</div>
+				</div>
 		</div>
 	</div>
 	
