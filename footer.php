a1efb4165ad9284d112e8746b4c529f6512b5c12
enoa7 <gamal.ardi@gmail.com>
2015-12-08 07:01:59 +0700

    fix client-logo-list footer

--- footer.php
+++ footer.php
@@ -1,18 +1,17 @@
-  <footer class="spacepad">
+<footer class="spacepad">
     <div class="container spacepad">
-    	<div class="footer-content">
-       <div class="client-logo-list">
-         <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/indosiar-logo.png" alt="indosiar logo"></div>
-         <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/liputan6_footer.png" alt="liputan6 logo"></div>
-         <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/vidio_footer.png" alt="vidio logo"></div>
-       </div>
-       <div class="copyright clearfix">
-         &copy;&nbsp; Copyright 2015 Indosiar
-       </div>
-      </div>
+        <div class="footer-content">
+            <div class="client-logo-list clearfix">
+                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/indosiar-logo.png" alt="indosiar logo"></div>
+                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/liputan6_footer.png" alt="liputan6 logo"></div>
+                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/vidio_footer.png" alt="vidio logo"></div>
+            </div>
+            <div class="copyright clearfix spacepad">
+                &copy;&nbsp; Copyright 2015 Indosiar
+            </div>
+        </div>
     </div>
-  </footer>
- <!-- /container -->
+</footer>
 
 <!-- all compiled scripts -->
 <script type="text/javascript" src="asset/main.min.js"></script>
