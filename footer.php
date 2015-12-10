35be9d90bb48151acea50ffc7a01e0390b9a58f8
Gamal Ardi Pratama <gamal.pratama@sctv-jkt.tv>
2015-12-10 16:37:35 +0700

    add links on the footer section

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
     <div class="container">
         <div class="footer-content">
             <div class="client-logo-list clearfix">
-                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/indosiar-logo.png" alt="indosiar logo"></div>
-                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/liputan6_footer.png" alt="liputan6 logo"></div>
-                <div class="col-xs-4"><img class="img-responsive center-block" src="asset/images/vidio_footer.png" alt="vidio logo"></div>
+                <div class="col-xs-4"><a target="_blank" href="http://www.indosiar.com"><img class="img-responsive center-block" src="asset/images/indosiar-logo.png" alt="indosiar logo"></a></div>
+                <div class="col-xs-4"><a target="_blank" href="http://www.liputan6.com/"><img class="img-responsive center-block" src="asset/images/liputan6_footer.png" alt="liputan6 logo"></a></div>
+                <div class="col-xs-4"><a target="_blank" href="http://www.vidio.com/@indosiar"_blank" "><img class="img-responsive center-block" src="asset/images/vidio_footer.png" alt="vidio logo"></a></div>
             </div>
             <div class="copyright clearfix spacepad">
                 &copy;&nbsp; Copyright 2015 Indosiar
