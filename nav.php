05d813b5a08ed27ba75d7bfc0a15074fd3cc8151
Gamal Ardi Pratama <gamal.pratama@sctv-jkt.tv>
2015-12-11 09:52:47 +0700

    change livestream url

--- nav.php
+++ nav.php
@@ -6,9 +6,9 @@
         <div class="nav-list col-sm-7 visible-lg">
             <ul class="nodots nopadding clearfix nomarginbottom menu-list">
                 <li><a href="/"><span>Beranda</span></a></li>
-                <li><a href="#livestream" class="smoothScroll"><span>Live Streaming</span></a></li>
+                <li><a href="index#livestream" class="smoothScroll"><span>Live Streaming</span></a></li>
                 <!-- <li><a href="#kontes"><span>Kontes</span></a></li> -->
-                <li><a href="kuis.php"><span>Kuis</span></a></li>
+                <li><a href="kuis"><span>Kuis</span></a></li>
                 <!-- <li><a href="#kegiatan-sosial"><span>Kegiatan Sosial</span></a></li> -->
                 <!-- <li><a href="#25-jam-live-nonstop"><span>25 jam live nonstop</span></a></li> -->
                 
