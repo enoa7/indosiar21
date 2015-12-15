d1d128c3399567abcc00e7df77ddd3b3109e4ee7
enoa <gamal.ardi@gmail.com>
2015-12-15 18:18:39 +0700

    add sosial page

---
+++ sosial.php
@@ -0,0 +1,20 @@
+<?php 
+
+	$pageTitle = "HUT Indosiar 21 | Kuis";
+	$pageSubject = "Kegiatan Sosial";
+	$pageKeywords = "indosiar, indosiar21, HUT, kegiatan sosial";
+	$pageDesc = "Kuis tebak program unggulan Indosiar dari masa ke masa dalam rangka HUT Indosiar ke 21";
+	$socialTitle = "Kuis Tebak Program HUT indosiar 21";
+	$socialDesc = "Kuis tebak program unggulan Indosiar dari masa ke masa dalam rangka HUT Indosiar ke 21";
+	include("header.php");
+
+?>
+<div class="page-wrap" id="kuis">
+	<div class="segment" id="sosial">
+	
+	</div>
+	<?php require('footer.php') ?>
+	<?php require('overlay-menu.php') ?>
+</div>
+</body>
+</html>
