2dd496c46fb6be7a19f2decd8d48979197ceb83e
enoa <gamal.ardi@gmail.com>
2015-12-16 17:32:03 +0700

    remove .php on sosial page

--- overlay-menu.php
+++ overlay-menu.php
@@ -5,7 +5,7 @@
 		<li><a href="kuis"><span>Kuis</span></a></li>
         <!-- <li><a href="#kontes"><span>Kontes</span></a></li> -->
         <!-- <li><a href="#kuis"><span>Kuis</span></a></li> -->
-        <li><a href="kegiatan-sosial.php"><span>Kegiatan Sosial</span></a></li>
+        <li><a href="kegiatan-sosial"><span>Kegiatan Sosial</span></a></li>
         <!-- <li><a href="#25-jam-live-nonstop"><span>25 jam live nonstop</span></a></li> -->
         
     </ul>
