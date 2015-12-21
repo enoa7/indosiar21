e1f8313ca324919898109b85fb4563e6dd3ce885
enoa <gamal.ardi@gmail.com>
2015-12-21 14:31:11 +0700

    add new list artist for 3rd lineup

--- index.php
+++ index.php
@@ -33,6 +33,22 @@
 		
 		<div class="container-fluid slicky">
 			<!-- list artist -->
+			<div class="slick-artist slank">
+				<div class="overlay-title">Konser Raya HUT 21 Indosiar Untuk Indonesia - 3rd Lineup </div>
+				<img class="img-responsive center-block" src="asset/images/list-artist/indosiar21-artist-slank.jpg" alt="2nd-lineup-citata">
+			</div>
+			<div class="slick-artist syahrini">
+				<div class="overlay-title">Konser Raya HUT 21 Indosiar Untuk Indonesia - 3rd Lineup </div>
+				<img class="img-responsive center-block" src="asset/images/list-artist/indosiar21-artist-syahrini.jpg" alt="2nd-lineup-citata">
+			</div>
+			<div class="slick-artist citata">
+				<div class="overlay-title">Konser Raya HUT 21 Indosiar Untuk Indonesia - 3rd Lineup </div>
+				<img class="img-responsive center-block" src="asset/images/list-artist/indosiar21-artist-citata.jpg" alt="2nd-lineup-citata">
+			</div>
+			<div class="slick-artist agnez">
+				<div class="overlay-title">Konser Raya HUT 21 Indosiar Untuk Indonesia - 3rd Lineup </div>
+				<img class="img-responsive center-block" src="asset/images/list-artist/indosiar21-artist-agnez.jpg" alt="2nd-lineup-agnez">
+			</div>
 			<div class="slick-artist cemenxdavid">
 				<div class="overlay-title">Konser Raya HUT 21 Indosiar Untuk Indonesia - 2nd Lineup </div>
 				<img class="img-responsive center-block" src="asset/images/list-artist/indosiar21-artist-cemenxdavid.jpg" alt="2nd-lineup-cemen dan david">
