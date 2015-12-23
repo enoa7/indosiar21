c039057af7cc9fbe28a4c6bb616b419cef0204dc
enoa <gamal.ardi@gmail.com>
2015-12-23 15:46:15 +0700

    make each result its own php page, fixes the layout of it and the twitter btn

--- cemen.php
+++ cemen.php
@@ -10,12 +10,27 @@
 ?>
 
 <?php require('nav.php') ?>
-<div class="container">
+<div class="container spacepad" id="kuis-result">
+<div class="col-xs-12 text-center"><h2 class="title">Kamu mirip ...</h2></div>
+<div class=" animated bounceInRight vertical-align-nofloat">
 
-<div class="spacepad animated bounceInRight vertical-align-nofloat"><div class="person-img col-xs-12 col-sm-6"><img class=img-responsive src=asset/images/kuis/personality/cemen.jpg alt="personality cemen"></div><div class="person-desc col-xs-12 col-sm-6"><h2>Cemen!</h2> Wah! Gaya pdkt kamu mirip banget nih sama Cemen karena kamu tuh rela melakukan apapun demi mendapatkan perhatian gebetan kamu. Dalam proses pdkt kamu akan sering kasih sinyal-sinyal positif ke gebetan kamu. Kamu akan mengatur strategi sedemikian rupa demi mendapat tanggapan dari gebetan kamu </div></div>
+<div class="person-img col-xs-12 col-sm-6"><img class=img-responsive src=asset/images/kuis/personality/cemen.jpg alt="personality cemen"></div>
+
+<div class="person-desc col-xs-12 col-sm-6"><h2>Cemen!</h2><p>Wah! Gaya pdkt kamu mirip banget nih sama Cemen karena kamu tuh rela melakukan apapun demi mendapatkan perhatian gebetan kamu. Dalam proses pdkt kamu akan sering kasih sinyal-sinyal positif ke gebetan kamu. Kamu akan mengatur strategi sedemikian rupa demi mendapat tanggapan dari gebetan kamu</p> 
 <div class="social-btn twitter right">
-<a href="https://twitter.com/share" class="twitter-share-button"{count} data-size="large">Tweet</a>
+
+<a href="https://twitter.com/share" data-text="Wah! gue mirip cemen! Yuk ikutan indosiar personality quiz di " data-url="http://hut.indosiar.com/personality-kuis" class="twitter-share-button"{count} data-size="large">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 
 </div>
 </div>
+
+</div>
+
+</div>
+
+	<?php require('footer.php') ?>
+	<?php require('overlay-menu.php') ?>
+	</div>
+</body>
+</html>
