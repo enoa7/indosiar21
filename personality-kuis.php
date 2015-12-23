20fd0ee6bb708a312ac78d0e3e101c3af5530f30
enoa <gamal.ardi@gmail.com>
2015-12-23 14:40:04 +0700

    change the twitter btn settings

--- personality-kuis.php
+++ personality-kuis.php
@@ -285,8 +285,8 @@
 
 			<div id="result" class="spacepad">
 			<div class="social-btn twitter right">
-				<a href="https://twitter.com/share" class="twitter-share-button"{count} data-url="http://hut.indosiar.com" data-text="Ayo ikutan kuis personality!" data-size="large" data-hashtags="menujuindosiar21">Tweet</a>
-				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+				<a href="https://twitter.com/share" class="twitter-share-button"{count} data-text="test" data-size="large">Tweet</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 			</div>
 
 			</div>
