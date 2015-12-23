89fd56ca02989d5e6d32c2e68b6ddf9148af0849
enoa <gamal.ardi@gmail.com>
2015-12-23 18:35:17 +0700

    add personality quiz on the nav

--- overlay-menu.php
+++ overlay-menu.php
@@ -3,10 +3,8 @@
         <li><a href="index"><span>Beranda</span></a></li>
 		<li><a href="index#livestream" class="smoothScroll"><span>Live Streaming</span></a></li>
 		<li><a href="kuis"><span>Kuis</span></a></li>
-        <!-- <li><a href="#kontes"><span>Kontes</span></a></li> -->
-        <!-- <li><a href="#kuis"><span>Kuis</span></a></li> -->
+		<li><a href="personality-quiz"><span>Personality Quiz</span></a></li>
         <li><a href="kegiatan-sosial"><span>Kegiatan Sosial</span></a></li>
-        <!-- <li><a href="#25-jam-live-nonstop"><span>25 jam live nonstop</span></a></li> -->
         
     </ul>
 </nav>
