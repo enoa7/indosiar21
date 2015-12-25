874ccbc25a8cfa7883d963dec34f5b68c6bbc874
enoa7 <gamal.ardi@gmail.com>
2015-12-25 08:20:20 +0700

    update kuis

--- kuis.php
+++ kuis.php
@@ -19,11 +19,11 @@
 		<h1>
 			<?php if ( $detect->isMobile() ) { ?>
 				<div class="slick-kuis">
-					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-9-mobile.jpg" alt="indosiar21 tebak program 7">
+					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-10-mobile.jpg" alt="indosiar21 tebak program 10">
 				</div>
 			<?php } else { ?>
 				<div class="slick-kuis">
-					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-9.jpg" alt="indosiar21 tebak program 7">
+					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-10.jpg" alt="indosiar21 tebak program 10">
 				</div>
 			<?php } ?>
 		</h1>
