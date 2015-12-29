4327efd8ed30da1b8dbedf3a4e2932d7ef97e42b
enoa7 <gamal.ardi@gmail.com>
2015-12-29 10:25:49 +0700

    add new kuis

--- kuis.php
+++ kuis.php
@@ -19,11 +19,11 @@
 		<h1>
 			<?php if ( $detect->isMobile() ) { ?>
 				<div class="slick-kuis">
-					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-13-mobile.jpg" alt="indosiar21 tebak program 13">
+					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-14-mobile.jpg" alt="indosiar21 tebak program 14">
 				</div>
 			<?php } else { ?>
 				<div class="slick-kuis">
-					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-13.jpg" alt="indosiar21 tebak program 13">
+					<img class="img-responsive center-block" src="asset/images/kuis/tebak-program/tebakprogram-14.jpg" alt="indosiar21 tebak program 14">
 				</div>
 			<?php } ?>
 		</h1>
