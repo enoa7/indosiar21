bad7e8b77bc4236b6c875d9b6ec20c9a45d2064a
enoa7 <gamal.ardi@gmail.com>
2016-01-04 21:12:00 +0700

    kegiatan-sosial.php => update intro text & saldo

--- kegiatan-sosial.php
+++ kegiatan-sosial.php
@@ -1,52 +1,70 @@
-<?php 
+<?php
 
-	$pageTitle = "HUT Indosiar 21 | Sekolah Kita";
-	$pageSubject = "Kegiatan Sosial";
-	$pageKeywords = "indosiar, indosiar21, HUT, kegiatan sosial, sekolah, kita";
-	$pageDesc = "Salurkan bantuan kamu untuk renovasi sekolah yang ke-5, yaitu SDN Rinca, Nusa Tenggara Timur, melalui rekening BCA 500 555 888 1.";
-	$socialShareImg = "http://hut.indosiar.com/asset/images/hutindosiar21.png";
-	include("header.php");
+    $pageTitle = 'HUT Indosiar 21 | Sekolah Kita';
+    $pageSubject = 'Kegiatan Sosial';
+    $pageKeywords = 'indosiar, indosiar21, HUT, kegiatan sosial, sekolah, kita';
+    $pageDesc = 'Salurkan bantuan kamu untuk renovasi sekolah yang ke-5, yaitu SDN Rinca, Nusa Tenggara Timur, melalui rekening BCA 500 555 888 1.';
+    $socialShareImg = 'http://hut.indosiar.com/asset/images/hutindosiar21.png';
+    include 'header.php';
 
 ?>
 <?php require('nav.php') ?>
 
-<div class="page-wrap" id="kuis">
-	<div class="segment" id="sosial">
+<div class="page-wrap" id="sosial">
+	<div class="segment">
 		<div class="main-banner">
 			<img class="img-responsive" src="asset/images/sekolah_kita.jpg" alt="sekolah kita indosiar 21">
 		</div>
 		<div class="container spacepad">
 			<div class="intro-text">
 				<h1 class="title text-center">Kegiatan Sosial</h1>
-				<p class="desc text-center hidden-xs">Hingga 16 Desember 2015, Sekolah Kita Indosiar telah berhasil mengumpulkan dana sebesar Rp 1.271.659.578.  Dana tersebut akan digunakan untuk renovasi empat sekolah, yaitu SDN Tenjolahang 2 Banten, SD Inpres Bengo 2 Gowa, SDN Trimulyo 2 Semarang, dan SMPN 1 Siberut Selatan, SDN Patemon 12, Probolinggo. Renovasi Sekolah Kita Indosiar saat ini telah dilakukan di dua sekolah, yaitu SDN Tenjolahang 2 Banten dan SD Inpres Bengo 2 Gowa. Perbaikan di SDN Tenjolahang 2, Banten, sudah sampai pada tahap akhir yaitu pemasangan atap dan keramik. Sementara itu, untuk SD Inpres Bengo 2 masih dalam proses penggalian pondasi untuk tiga ruang kelas.</p>
-				<p class="text-center mobile-text">	
+				<!-- <p class="desc text-center hidden-xs">Hingga 16 Desember 2015, Sekolah Kita Indosiar telah berhasil mengumpulkan dana sebesar Rp 1.271.659.578.  Dana tersebut akan digunakan untuk renovasi empat sekolah, yaitu SDN Tenjolahang 2 Banten, SD Inpres Bengo 2 Gowa, SDN Trimulyo 2 Semarang, dan SMPN 1 Siberut Selatan, SDN Patemon 12, Probolinggo. Renovasi Sekolah Kita Indosiar saat ini telah dilakukan di dua sekolah, yaitu SDN Tenjolahang 2 Banten dan SD Inpres Bengo 2 Gowa. Perbaikan di SDN Tenjolahang 2, Banten, sudah sampai pada tahap akhir yaitu pemasangan atap dan keramik. Sementara itu, untuk SD Inpres Bengo 2 masih dalam proses penggalian pondasi untuk tiga ruang kelas.</p>
+				<p class="text-center mobile-text">
 					Salurkan terus bantuan kamu untuk renovasi sekolah yang ke-6, yaitu SDN Pulau Rinca, Desa Pasir Panjanga, Kec. Komodo, Kab. Mangarai Barat, Nusa Tenggara Timur melalui rekening BCA 500-555-888-1.
+				</p> -->
+				<p class="desc text-center hidden-xs">
+					Dalam rangka hut indosiar ke 21, program‎ "sekolah kita indosiar" ‎ upaya mewujudkan harapan anak-anak kita murid SDN pulau rinca, desa pasir panjang, kec. komodo, kab. mangarai barat, nusa tenggara timur  akan sebuah sekolah yang nyaman & aman untuk belajar, dengan menggalang dana  Rp 426.953.100,-  untuk renovasi sekolah mereka.  saat ini dana yang baru terkumpul sebesar  Rp. 219,512,036,- mari salurkan bantuan anda lewat rekening bca, no rek. 500 555 888 1  a/n sekolah kita indosiar (pt indosiar visual mandiri). kepedulian kita untuk masa depan mereka.
+				</p>
+				<p class="desc text-center hidden-xs">
+					Program sekolah kita indosiar akan berakhir 7 hari lagi, tanggal 11 januari 2016 pukul 23:59 WIB
+				</p>
+				<p class="desc text-center hidden-xs">
+					Total  dana “sekolah kita indosiar”  yang terkumpul sampai 31 desemeber 2015, sebesar <span class="highlight">Rp. 1,628,986,611,- </span>dan  telah disalurkan untuk 5 sekolah :
+					SDN tenjolahang 2 pandeglang proses renovasi sudah 95% saat ini sedang finishing & menunggu meubel, SD inpres bengo 2 gowa sedang proses pondasi dan pembesian,
+					SDN trimulyo 2 semarang sedang proses pembongkaran bangunan yang rusak, smpn 1 siberut selatan dan  SDN Patemon 12, probolinggo  baru akan memulai proses renovasi.
+				</p>
+				<p class="desc text-center hidden-xs">
+					Terima kasih sebesar-besarnya kepada seluruh pemirsa yang sudah menyumbangkan dana. semoga tuhan yang maha esa membalas semua bantuan dan kebaikan pemirsa.
 				</p>
 			</div>
 				<div class="saldo col-xs-12">
 					<div class="name col-xs-12 col-sm-6">total saldo</div>
-					<div class="amount col-xs-12 col-sm-6">Rp 1.555.666.886,-</div>
+					<div class="amount col-xs-12 col-sm-6">Rp 1,628,986,611,-</div>
 				</div>
 			<div class="activity one clearfix">
 				<h3 class="title">SDN Tenjolahang 2, Jiput - Pandeglang Banten</h3>
 				<div class="video col-xs-12 text-center">
 					<div class="">
-						<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-sdn-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
+						<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-SDN-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
-				<?php if( $detect->isMobile() && !$detect->isTablet() ){ ?>
+				<?php if ($detect->isMobile() && !$detect->isTablet()) {
+    ?>
 					<div class="gallery mobile col-xs-12">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-1.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-2.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-3.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-1.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-2.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-3.jpg" alt="">
 					</div>
-				<?php } else { ?>
+				<?php
+} else {
+    ?>
 					<div class="gallery desktop col-xs-12">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-1.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-2.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-3.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-1.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-2.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-3.jpg" alt="">
 					</div>
-				<?php } ?>
+				<?php
+} ?>
 
 			</div>
 			<hr>
@@ -62,24 +80,28 @@
 				    <span class="">10% progress</span>
 				  </div>
 				</div> -->
-				<?php if( $detect->isMobile() && !$detect->isTablet() ){ ?>
+				<?php if ($detect->isMobile() && !$detect->isTablet()) {
+    ?>
 					<div class="gallery mobile col-xs-12">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-1.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-2.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-3.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-4.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-1.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-2.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-3.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-4.jpg" alt="">
 					</div>
-				<?php } else { ?>
+				<?php
+} else {
+    ?>
 					<div class="gallery desktop col-xs-12">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-1.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-2.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-3.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-4.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-1.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-2.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-3.jpg" alt="">
+					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-4.jpg" alt="">
 					</div>
-				<?php } ?>
+				<?php
+} ?>
 			</div>
 			<!-- <div class="activity clearfix">
-				<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-sdn-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&player_only=false&" width="560" height="508" scrolling="no" frameborder="0" allowfullscreen></iframe>
+				<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-SDN-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&player_only=false&" width="560" height="508" scrolling="no" frameborder="0" allowfullscreen></iframe>
 			</div> -->
 		</div>
 	</div>
