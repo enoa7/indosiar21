a1f2f91b3bb374845f4417e0ccea498beafae4dd
enoa7 <gamal.ardi@gmail.com>
2016-01-05 11:37:18 +0700

    kegiatan-sosial.php => add more images, add 2 new schools

--- kegiatan-sosial.php
+++ kegiatan-sosial.php
@@ -3,7 +3,7 @@
     $pageTitle = 'HUT Indosiar 21 | Sekolah Kita';
     $pageSubject = 'Kegiatan Sosial';
     $pageKeywords = 'indosiar, indosiar21, HUT, kegiatan sosial, sekolah, kita';
-    $pageDesc = 'Salurkan bantuan kamu untuk renovasi sekolah yang ke-5, yaitu SDN Rinca, Nusa Tenggara Timur, melalui rekening BCA 500 555 888 1.';
+    $pageDesc = 'Salurkan bantuan kamu untuk renovasi sekolah yang ke-5, yaitu sdn Rinca, Nusa Tenggara Timur, melalui rekening BCA 500 555 888 1.';
     $socialShareImg = 'http://hut.indosiar.com/asset/images/hutindosiar21.png';
     include 'header.php';
 
@@ -18,18 +18,14 @@
 		<div class="container spacepad">
 			<div class="intro-text">
 				<h1 class="title text-center">Kegiatan Sosial</h1>
-				<!-- <p class="desc text-center hidden-xs">Hingga 16 Desember 2015, Sekolah Kita Indosiar telah berhasil mengumpulkan dana sebesar Rp 1.271.659.578.  Dana tersebut akan digunakan untuk renovasi empat sekolah, yaitu SDN Tenjolahang 2 Banten, SD Inpres Bengo 2 Gowa, SDN Trimulyo 2 Semarang, dan SMPN 1 Siberut Selatan, SDN Patemon 12, Probolinggo. Renovasi Sekolah Kita Indosiar saat ini telah dilakukan di dua sekolah, yaitu SDN Tenjolahang 2 Banten dan SD Inpres Bengo 2 Gowa. Perbaikan di SDN Tenjolahang 2, Banten, sudah sampai pada tahap akhir yaitu pemasangan atap dan keramik. Sementara itu, untuk SD Inpres Bengo 2 masih dalam proses penggalian pondasi untuk tiga ruang kelas.</p>
-				<p class="text-center mobile-text">
-					Salurkan terus bantuan kamu untuk renovasi sekolah yang ke-6, yaitu SDN Pulau Rinca, Desa Pasir Panjanga, Kec. Komodo, Kab. Mangarai Barat, Nusa Tenggara Timur melalui rekening BCA 500-555-888-1.
-				</p> -->
 				<p class="desc text-center hidden-xs">
-					Dalam rangka hut indosiar ke 21, program‎ "Sekolah Kita Indosiar" ‎upaya mewujudkan harapan anak-anak kita murid SDN Pulau Rinca, Desa Pasir Panjang, Kec. Komodo, Kab. Manggarai Barat, Nusa Tenggara Timur akan sebuah sekolah yang nyaman & aman untuk belajar, dengan menggalang dana  Rp 426.953.100,-  untuk renovasi sekolah mereka.  Saat ini dana yang baru terkumpul sebesar  Rp. 219,512,036,- mari salurkan bantuan anda lewat rekening bca, no rek. 500 555 888 1  a/n sekolah kita Indosiar (PT Indosiar Visual Mandiri). Kepedulian kita untuk masa depan mereka.
+					Dalam rangka hut indosiar ke 21, program‎ "Sekolah Kita Indosiar" ‎upaya mewujudkan harapan anak-anak kita murid sdn Pulau Rinca, Desa Pasir Panjang, Kec. Komodo, Kab. Manggarai Barat, Nusa Tenggara Timur akan sebuah sekolah yang nyaman & aman untuk belajar, dengan menggalang dana  Rp 426.953.100,-  untuk renovasi sekolah mereka.  Saat ini dana yang baru terkumpul sebesar  Rp. 219,512,036,- mari salurkan bantuan anda lewat rekening bca, no rek. 500 555 888 1  a/n sekolah kita Indosiar (PT Indosiar Visual Mandiri). Kepedulian kita untuk masa depan mereka.
 				</p>
 				<p class="desc text-center hidden-xs">
 					Program sekolah kita Indosiar akan berakhir 7 hari lagi, tanggal 11 januari 2016 pukul 23:59 WIB
 				</p>
 				<p class="desc text-center hidden-xs">
-					Total dana "Sekolah Kita Indosiar" yang terkumpul sampai 31 Desember 2015, sebesar <span class="highlight">Rp. 1,628,986,611,- </span>dan  telah disalurkan untuk 5 sekolah; SDN Tenjolahang 2 Pandeglang proses renovasi sudah 95% saat ini sedang finishing & menunggu meubel, SD Inpres Bengo 2 Gowa sedang proses pondasi dan pembesian, SDN Trimulyo 2 Semarang sedang proses pembongkaran bangunan yang rusak, SMPN 1 Siberut Selatan dan SDN Patemon 12, Probolinggo baru akan memulai proses renovasi.
+					Total dana "Sekolah Kita Indosiar" yang terkumpul sampai 31 Desember 2015, sebesar <span class="highlight">Rp. 1,628,986,611,- </span>dan  telah disalurkan untuk 5 sekolah; sdn Tenjolahang 2 Pandeglang proses renovasi sudah 95% saat ini sedang finishing & menunggu meubel, SD Inpres Bengo 2 Gowa sedang proses pondasi dan pembesian, sdn Trimulyo 2 Semarang sedang proses pembongkaran bangunan yang rusak, SMPN 1 Siberut Selatan dan sdn Patemon 12, Probolinggo baru akan memulai proses renovasi.
 				</p>
 				<p class="desc text-center hidden-xs">
 					Terima kasih sebesar-besarnya kepada seluruh pemirsa yang sudah menyumbangkan dana. semoga Tuhan Yang Maha Esa membalas semua bantuan dan kebaikan pemirsa.
@@ -40,30 +36,37 @@
 					<div class="amount col-xs-12 col-sm-6">Rp 1,628,986,611,-</div>
 				</div>
 			<div class="activity one clearfix">
-				<h3 class="title">SDN Tenjolahang 2, Jiput - Pandeglang Banten</h3>
+				<h3 class="title">sdn Tenjolahang 2, Jiput - Pandeglang Banten</h3>
 				<div class="video col-xs-12 text-center">
 					<div class="">
-						<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-SDN-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
+						<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-sdn-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
 				<?php if ($detect->isMobile() && !$detect->isTablet()) {
     ?>
 					<div class="gallery mobile col-xs-12">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-1.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-2.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-3.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-1.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-2.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-3.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-5.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-6.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-7.jpg" alt="">
 					</div>
 				<?php
 } else {
     ?>
 					<div class="gallery desktop col-xs-12">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-1.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-2.jpg" alt="">
-						<img class="img-responsive" src="asset/images/activity/SDN-tenjolahang-3.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-1.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-2.jpg" alt="">
+						<img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-3.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-5.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-6.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-tenjolahang-7.jpg" alt="">
 					</div>
 				<?php
 } ?>
-
 			</div>
 			<hr>
 			<div class="activity two clearfix">
@@ -73,35 +76,78 @@
 						<iframe class="vidio-embed" src="http://www.vidio.com/embed/184801-sd-inpres-bengo-2-sulawesi-selatan-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
-				<!-- <div class="progress col-xs-12 nopadding hidden-xs">
-				  <div class="progress-bar" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 15%;">
-				    <span class="">10% progress</span>
-				  </div>
-				</div> -->
 				<?php if ($detect->isMobile() && !$detect->isTablet()) {
     ?>
 					<div class="gallery mobile col-xs-12">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-1.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-2.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-3.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-1.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-2.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-3.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-5.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-6.jpg" alt="">
 					</div>
 				<?php
 } else {
     ?>
 					<div class="gallery desktop col-xs-12">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-1.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-2.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-3.jpg" alt="">
-					<img class="img-responsive" src="asset/images/activity/SDN-bengo-gowa-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-1.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-2.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-3.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-4.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-5.jpg" alt="">
+            <img class="img-responsive" src="asset/images/activity/sdn-bengo-gowa-6.jpg" alt="">
 					</div>
 				<?php
 } ?>
 			</div>
-			<!-- <div class="activity clearfix">
-				<iframe class="vidio-embed" src="http://www.vidio.com/embed/166927-SDN-tenjolahang-2-pandeglang-sekolah-kita?autoplay=false&player_only=false&" width="560" height="508" scrolling="no" frameborder="0" allowfullscreen></iframe>
-			</div> -->
 		</div>
+    			<hr>
+    <div class="activity three clearfix">
+      <h3 class="title">SD Rinca Manggarai, Nusa Tenggara Timur</h3>
+      <div class="video col-xs-12 text-center">
+        <div class="hide">
+          <iframe class="vidio-embed" src="http://www.vidio.com/embed/184801-sd-inpres-bengo-2-sulawesi-selatan-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <?php if ($detect->isMobile() && !$detect->isTablet()) { ?>
+        <div class="gallery mobile col-xs-12">
+        <img class="img-responsive" src="asset/images/activity/sdn-rinca-1.jpg" alt="">
+        <img class="img-responsive" src="asset/images/activity/sdn-rinca-2.jpg" alt="">
+        <img class="img-responsive" src="asset/images/activity/sdn-rinca-3.jpg" alt="">
+        <img class="img-responsive" src="asset/images/activity/sdn-rinca-4.jpg" alt="">
+        <img class="img-responsive" src="asset/images/activity/sdn-rinca-5.jpg" alt="">
+        </div>
+      <?php } else { ?>
+        <div class="gallery desktop col-xs-12">
+          <img class="img-responsive" src="asset/images/activity/sdn-rinca-1.jpg" alt="">
+          <img class="img-responsive" src="asset/images/activity/sdn-rinca-2.jpg" alt="">
+          <img class="img-responsive" src="asset/images/activity/sdn-rinca-3.jpg" alt="">
+          <img class="img-responsive" src="asset/images/activity/sdn-rinca-4.jpg" alt="">
+          <img class="img-responsive" src="asset/images/activity/sdn-rinca-5.jpg" alt="">
+        </div>
+      <?php } ?>
+    </div>
+    <hr>
+    <div class="activity four clearfix">
+      <h3 class="title">SDN Trimulyo 2, Semarang</h3>
+      <div class="video col-xs-12 text-center">
+        <div class="hide">
+          <iframe class="vidio-embed" src="http://www.vidio.com/embed/184801-sd-inpres-bengo-2-sulawesi-selatan-sekolah-kita?autoplay=false&" scrolling="no" frameborder="0" allowfullscreen></iframe>
+        </div>
+      </div>
+      <?php if ($detect->isMobile() && !$detect->isTablet()) { ?>
+        <div class="gallery mobile col-xs-12">
+        <img class="img-responsive" src="asset/images/activity/sdn-trimulyo-1.jpg" alt="">
+        <img class="img-responsive" src="asset/images/activity/sdn-trimulyo-2.jpg" alt="">
+        </div>
+      <?php } else { ?>
+        <div class="gallery desktop col-xs-12">
+          <img class="img-responsive" src="asset/images/activity/sdn-trimulyo-1.jpg" alt="">
+          <img class="img-responsive" src="asset/images/activity/sdn-trimulyo-2.jpg" alt="">
+        </div>
+      <?php } ?>
+    </div>
+  </div>
 	</div>
 	<?php require('footer.php') ?>
 	<?php require('overlay-menu.php') ?>
