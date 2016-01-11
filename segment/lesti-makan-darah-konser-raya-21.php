a0c9a3dabbf288bf61802b358c568bbb3a67db09
enoa7 <gamal.ardi@gmail.com>
2016-01-11 21:02:31 +0700

    add more 6 vod

---
+++ segment/lesti-makan-darah-konser-raya-21.php
@@ -0,0 +1,31 @@
+<?php
+
+	$string = "lesti-makan-darah-konser-raya-21";
+	$hometitle = str_replace("-", " ", $string);
+	$hometitle = ucfirst($hometitle);
+	$pageTitle = "HUT Indosiar 21 | VoD - " . $hometitle . "";
+	$pageSubject = "Konser Raya HUT 21 Indosiar Untuk Indonesia";
+	$pageKeywords = "hut indosiar 21, video on demand";
+	$pageDesc = $hometitle;
+	$socialShareImg = "http://hut.indosiar.com/asset/images/vod/vod-thumb-9.png";
+	require('../segment-header.php')
+?>
+
+
+<?php require('../segment-nav.php') ?>
+
+<!-- this is the index page -->
+<div class="page-wrap vod" id="<?php echo $string; ?>">
+	<div class="content">
+		<div class="container">
+			<h2 class="title text-center"><?php echo $hometitle; ?></h2>
+			<div class="video col-xs-12 spacepad">
+				<iframe class="vidio-embed" src="http://www.vidio.com/embed/244506-lesti-makan-darah-konser-raya-21?autoplay=false&amp;player_only=true&amp;" scrolling="no" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>	
+	</div>	
+	<?php require('../segment-footer.php') ?>
+	<?php require('../segment-overlaymenu.php') ?>
+</div>
+</body>
+</html>
---
+++ segment/raditya-dika-cinta-sama-indosiar-konser-raya-21.php
@@ -0,0 +1,31 @@
+<?php
+
+	$string = "raditya-dika-pendekar-cinta-kandas-konser-raya-21";
+	$hometitle = str_replace("-", " ", $string);
+	$hometitle = ucfirst($hometitle);
+	$pageTitle = "HUT Indosiar 21 | VoD - " . $hometitle . "";
+	$pageSubject = "Konser Raya HUT 21 Indosiar Untuk Indonesia";
+	$pageKeywords = "hut indosiar 21, video on demand";
+	$pageDesc = $hometitle;
+	$socialShareImg = "http://hut.indosiar.com/asset/images/vod/vod-thumb-7.png";
+	require('../segment-header.php')
+?>
+
+
+<?php require('../segment-nav.php') ?>
+
+<!-- this is the index page -->
+<div class="page-wrap vod" id="<?php echo $string; ?>">
+	<div class="content">
+		<div class="container">
+			<h2 class="title text-center"><?php echo $hometitle; ?></h2>
+			<div class="video col-xs-12 spacepad">
+				<iframe class="vidio-embed" src="http://www.vidio.com/embed/244503-raditya-dika-cinta-sama-indosiar-konser-raya-21?autoplay=false&amp;player_only=true&amp;" scrolling="no" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</div>	
+	</div>	
+	<?php require('../segment-footer.php') ?>
+	<?php require('../segment-overlaymenu.php') ?>
+</div>
+</body>
+</html>
